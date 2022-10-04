885a1079b827c5fd7119a63a5df4489a6bbfe2c6
Oli Iliffe <oliver.iliffe@kcl.ac.uk>
2022-10-04 21:21:54 +0100

    update to print median

--- median.py
+++ median.py
@@ -1,12 +1,26 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+def is_even(n: int) -> bool:
+    """
+    returns `True` if the provided `int` is even, otherwise
+    returns `False`
+    """    
+    return n % 2 == 0
 
-while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
-    else:
-        break
-print(numbers)
+if __name__ == "__main__":
+    while True:
+        try:
+            user_input = input("Enter a list of numbers separated by commas:\n")
+            if user_input == "exit()": break
+
+            numbers = [float(n) for n in user_input.split(",")]
+            numbers.sort()
+
+            middle_index = len(numbers) // 2;
+            if is_even(len(numbers)):
+                median = (numbers[middle_index] + numbers[middle_index - 1]) / 2
+            else:
+                median = numbers[middle_index]
+                
+            print(f"median = {median}")
+        except Exception as e: 
+            print(e)
+        else: break
